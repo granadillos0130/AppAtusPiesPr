3eb1b2cfd2a56f8912daa66d84550879c0ea64ee
Luis Salamanca <luisguezguann@gmail.com>
2024-11-17 12:17:49 -0500

    Creo las Entidades del Producto

--- AppAtusPiesPr/Entidades/ClProductoE.cs
+++ AppAtusPiesPr/Entidades/ClProductoE.cs
@@ -7,5 +7,22 @@ namespace AppAtusPiesPr.Entidades
 {
     public class ClProductoE
     {
+        public int idProducto {  get; set; }
+
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int CantidadStock { get; set; }
+
+        public int Precio { get; set; }
+
+        public string Presentacion { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Talla { get; set; }
+
+        public int idVendedor { get; set; }
     }
 }
