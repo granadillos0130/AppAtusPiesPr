9e5f372acfc1a26aa29a74de06d737bb75c43116
Luis Salamanca <luisguezguann@gmail.com>
2024-11-17 13:41:01 -0500

    Modifique el archivo ProductoRegistro

--- AppAtusPiesPr/Vista/ProductoRegistro.aspx.designer.cs
+++ AppAtusPiesPr/Vista/ProductoRegistro.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace AppAtusPiesPr.Vista
     {
 
         /// <summary>
-        /// Control formRegistrarProducto.
+        /// Control ScriptManager1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm formRegistrarProducto;
+        protected global::System.Web.UI.ScriptManager ScriptManager1;
 
         /// <summary>
         /// Control txtNombre.
