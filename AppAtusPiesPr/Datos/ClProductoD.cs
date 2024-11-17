cec5b1fef53a9eca9bf39a2bfed95747f0842d20
Luis Salamanca <luisguezguann@gmail.com>
2024-11-17 12:59:36 -0500

    Implemento método MtdRegistrarProducto para registrar productos en la base de datos utilizando parámetros como código, nombre, cantidad, precio, presentación, talla e idVendedor.

--- AppAtusPiesPr/Datos/ClProductoD.cs
+++ AppAtusPiesPr/Datos/ClProductoD.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppAtusPiesPr.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +10,24 @@ namespace AppAtusPiesPr.Datos
 {
     public class ClProductoD
     {
+        public ClProductoE MtdRegistrarProducto(ClProductoE objdata)
+        {
+            ClConexion objConexion = new ClConexion();
+            SqlCommand cmd = new SqlCommand("insertarProducto",objConexion.MtdAbrirConexion());
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@codigo",objdata.Codigo);
+            cmd.Parameters.AddWithValue("@nombre",objdata.Nombre);
+            cmd.Parameters.AddWithValue("@cantidadStock",Convert.ToInt32(objdata.CantidadStock));
+            cmd.Parameters.AddWithValue("@precio", Convert.ToInt32(objdata.Precio));
+            cmd.Parameters.AddWithValue("@presentacion",objdata.Presentacion.ToString());
+            cmd.Parameters.AddWithValue("@talla",objdata.Talla);
+            cmd.Parameters.AddWithValue("@idvendedor",Convert.ToInt32(objdata.idVendedor));
+
+            cmd.ExecuteNonQuery();
+            objConexion.MtdCerrarConexion();
+            return objdata;
+        }
     }
 }
