f2d809611a4dd3968c7886035990d3261980afca
Luis Salamanca <luisguezguann@gmail.com>
2024-11-17 21:59:10 -0500

    Corrijo errores en el campo estado, ademas incluyo un parseo en los que estan en entero

--- AppAtusPiesPr/Vista/ActualizarProducto.aspx.cs
+++ AppAtusPiesPr/Vista/ActualizarProducto.aspx.cs
@@ -21,6 +21,14 @@ namespace AppAtusPiesPr.Vista
         {
             try
             {
+
+                // Validar el ID del producto
+                if (!int.TryParse(txtProducto.Text, out int idProducto))
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "alertMessage",
+                        "Swal.fire('Error', 'El ID del producto debe ser un número válido', 'error');", true);
+                    return;
+                }
                 string rutaImagen = null;
 
                 // Procesar la imagen si se ha subido una nueva
@@ -32,17 +40,17 @@ namespace AppAtusPiesPr.Vista
                     if (extensionesPermitidas.Contains(extension))
                     {
                         string nombreArchivo = Guid.NewGuid().ToString() + extension;
-                        rutaImagen = Server.MapPath("~/img/Productos/") + nombreArchivo;
+                        rutaImagen = Server.MapPath("~/img/Producto/") + nombreArchivo;
 
                         // Crear directorio si no existe
-                        if (!Directory.Exists(Server.MapPath("~/img/Productos/")))
+                        if (!Directory.Exists(Server.MapPath("~/img/Producto/")))
                         {
-                            Directory.CreateDirectory(Server.MapPath("~/img/Productos/"));
+                            Directory.CreateDirectory(Server.MapPath("~/img/Producto/"));
                         }
 
                         // Guardar la imagen
                         inRuta.SaveAs(rutaImagen);
-                        rutaImagen = "~/img/Productos/" + nombreArchivo;
+                        rutaImagen = "~/img/Producto/" + nombreArchivo;
                     }
                     else
                     {
@@ -55,14 +63,14 @@ namespace AppAtusPiesPr.Vista
                 // Crear objeto con los datos del producto
                 ClProductoE objProductoE = new ClProductoE
                 {
-                    idProducto = int.Parse(txtProducto.Text),
+                    idProducto = int.TryParse(txtProducto.Text, out int id) ? id : 0,
                     Nombre = txtNombre.Text,
                     Codigo = txtCodigo.Text,
-                    CantidadStock = int.Parse(txtStock.Text),
+                    CantidadStock = int.TryParse(txtStock.Text, out int stock) ? stock : 0,
                     Estado = txtEstado.Text,
-                    Precio = int.Parse(txtPrecio.Text),
+                    Precio = int.TryParse(txtPrecio.Text, out int precio) ? precio : 0,
                     Talla = txtTalla.Text,
-                    Presentacion = rutaImagen // Solo se actualiza si se subió una nueva imagen
+                    Presentacion = rutaImagen// Solo se actualiza si se subió una nueva imagen
                 };
 
                 // Llamar al método de actualización
