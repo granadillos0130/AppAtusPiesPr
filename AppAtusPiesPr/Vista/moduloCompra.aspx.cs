d4ce9cafddd0736822525adced9df4c8fcdc000f
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2024-11-17 21:25:42 -0500

    creacion del modulo de informacion de los productos, en la capa vista

---
+++ AppAtusPiesPr/Vista/moduloCompra.aspx.cs
@@ -0,0 +1,91 @@
+﻿using AppAtusPiesPr.Entidades;
+using AppAtusPiesPr.Logica;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppAtusPiesPr.Vista
+{
+    public partial class moduloCompra : System.Web.UI.Page
+    {
+        private ClProductoL oLogica = new ClProductoL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                cargarProductos();
+                cargarCategorias();
+
+                string productoId = Request.QueryString["id"];
+                if (!string.IsNullOrEmpty(productoId) && int.TryParse(productoId, out int id))
+                {
+                    cargarProducto(id);
+
+                }
+            }
+        }
+
+        private void cargarProducto(int idProducto)
+        {
+            ClProductoEmpresaE oProductoE = oLogica.MtdInfoProductos(idProducto);
+
+            if (oProductoE != null)
+            {
+                lblTituloProducto.Text = oProductoE.nombreProducto;
+                ImgProducto.ImageUrl = oProductoE.imagen;
+
+                // Conversión de int a string para asignar a las etiquetas de texto
+                stockProducto.Text = oProductoE.cantidadStock.ToString();
+                referenciaProducto.Text = oProductoE.referencia;
+                descuento.Text = oProductoE.descuento.ToString();
+                nombreVendedor.Text = oProductoE.nombres;
+                PrecioProducto.Text = oProductoE.precioVenta.ToString();
+                marcaProducto.Text = oProductoE.nombreMarca;
+                productoDescripcion.Text = oProductoE.descripcionProducto;
+
+                if (oProductoE.TallasDisponibles != null && oProductoE.TallasDisponibles.Count > 0)
+                {
+                    ddlTallas.DataSource = oProductoE.TallasDisponibles;
+                    ddlTallas.DataTextField = "descripcionTalla";
+                    ddlTallas.DataTextField = "idTalla";
+                    ddlTallas.DataBind();
+                    ddlTallas.Items.Insert(0, new ListItem("Tallas Disponibles", "0"));
+                }
+                else
+                {
+                    ddlTallas.Items.Clear();
+                    ddlTallas.Items.Insert(0, new ListItem("No Hay Tallas Disponibles", "0"));
+                }
+            }
+            else
+            {
+                lblMensaje.InnerText = "No se encontró la información del candidato.";
+            }
+        }
+
+        private void cargarProductos()
+        {
+            ClProductoL objProductoL = new ClProductoL();
+            DataTable dt = objProductoL.MtdListarProductos();
+            Repeater1.DataSource = dt;
+            Repeater1.DataBind();
+        }
+
+        private void cargarCategorias()
+        {
+            ClProductoL oLogica = new ClProductoL();
+            Repeater2.DataSource = oLogica.MtdListarCategorias();
+            Repeater2.DataBind();
+        }
+
+        protected void btnAgregarCarrito_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ AppAtusPiesPr/Vista/moduloCompra.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace AppAtusPiesPr.Vista
+{
+
+
+    public partial class moduloCompra
+    {
+
+        /// <summary>
+        /// Control Repeater2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater2;
+
+        /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lblMensaje;
+
+        /// <summary>
+        /// Control ImgProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image ImgProducto;
+
+        /// <summary>
+        /// Control lblTituloProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTituloProducto;
+
+        /// <summary>
+        /// Control nombreVendedor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label nombreVendedor;
+
+        /// <summary>
+        /// Control stockProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label stockProducto;
+
+        /// <summary>
+        /// Control referenciaProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label referenciaProducto;
+
+        /// <summary>
+        /// Control descuento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label descuento;
+
+        /// <summary>
+        /// Control productoDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label productoDescripcion;
+
+        /// <summary>
+        /// Control ddlTallas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlTallas;
+
+        /// <summary>
+        /// Control marcaProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label marcaProducto;
+
+        /// <summary>
+        /// Control PrecioProducto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label PrecioProducto;
+
+        /// <summary>
+        /// Control btnAgregarCarrito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarCarrito;
+
+        /// <summary>
+        /// Control Repeater1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+    }
+}
