95ead53396d9b2115497a691948d88cd156c9e70
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2024-11-18 08:48:12 -0500

    sss

---
+++ AppAtusPiesPr/index.aspx.cs
@@ -0,0 +1,39 @@
+﻿using AppAtusPiesPr.Logica;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AppAtusPiesPr
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                cargarProductos();
+                cargarCategorias();
+            }
+        }
+
+        private void cargarProductos()
+        {
+            ClProductoL objProductoL = new ClProductoL();
+            DataTable dt = objProductoL.MtdListarProductos();
+            Repeater1.DataSource = dt;
+            Repeater1.DataBind();
+        }
+
+        private void cargarCategorias()
+        {
+            ClProductoL oLogica = new ClProductoL();
+            Repeater2.DataSource = oLogica.MtdListarCategorias();
+            Repeater2.DataBind();
+        }
+
+    }
+}
