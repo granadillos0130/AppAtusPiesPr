72a485a5f7b5d3e85481aa4a7746e1c214901a8d
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2024-11-18 20:57:06 -0500

    modificaciones del modulo de filtro por categoria

--- AppAtusPiesPr/Vista/moduloCatalogoFiltrado.aspx.designer.cs
+++ AppAtusPiesPr/Vista/moduloCatalogoFiltrado.aspx.designer.cs
@@ -22,5 +22,23 @@ namespace AppAtusPiesPr.Vista
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Repeater2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater2;
+
+        /// <summary>
+        /// Control Repeater1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
     }
 }
