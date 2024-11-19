1f0ff66b415484413ae4afe5c50fa40ecce73261
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2024-11-18 19:15:38 -0500

    creacion mtd listar por filtro de categoria en logica

--- AppAtusPiesPr/Logica/ClProductoL.cs
+++ AppAtusPiesPr/Logica/ClProductoL.cs
@@ -53,6 +53,12 @@ namespace AppAtusPiesPr.Logica
             return objActuP.mtdActualizarProducto(objProdActu);
         }
 
+        public List<ClProductoEmpresaE> MtdListarPorFiltro(int idCategoria)
+        {
+            ClProductoL objProL = new ClProductoL();
+            List<ClProductoEmpresaE> tblDatos = objProL.MtdListarPorFiltro(idCategoria);
+            return tblDatos;
+        }
 
     }
 }
