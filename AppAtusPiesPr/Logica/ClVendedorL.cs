a8f127bb0e95cfd7adb24292569ca4c336d4472f
Brayam96 <165792204+Brayam96@users.noreply.github.com>
2024-11-21 08:12:06 -0500

    sss

--- AppAtusPiesPr/Logica/ClVendedorL.cs
+++ AppAtusPiesPr/Logica/ClVendedorL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppAtusPiesPr.Datos;
+using AppAtusPiesPr.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,14 @@ namespace AppAtusPiesPr.Logica
 {
     public class ClVendedorL
     {
+        ClVendedorD oVendedor = new ClVendedorD();
+        public int RegistrarVendedor(ClUsuarioE vendedor)
+        {
+            return oVendedor.MtdRegistrarVendedor(vendedor);
+        }
+
+
+
+
     }
 }
