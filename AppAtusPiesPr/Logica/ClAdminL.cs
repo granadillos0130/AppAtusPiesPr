8f97ea4fca4629f0ad69b6227ae0a20985dc5525
PC_15 <granadossantiago2006@gmail.com>
2024-11-21 08:04:31 -0500

    Eliminacion de los metodos de Registrar Vendedores y la encriptacion de la contraseña en ClAdminL

--- AppAtusPiesPr/Logica/ClAdminL.cs
+++ AppAtusPiesPr/Logica/ClAdminL.cs
@@ -12,27 +12,6 @@ namespace AppAtusPiesPr.Logica
 {
     public class ClAdminL
     {
-        public ClUsuarioE MtdRegistrarVendedor(ClUsuarioE oDatos)
-        {
-            oDatos.Password = HashPassword(oDatos.Password);
-
-            ClAdminD oDatosD = new ClAdminD();
-            return oDatosD.MtdRegistrarVendedor(oDatos);
-        }
-
-        private string HashPassword(string password)
-        {
-            using (SHA256 sha256 = SHA256.Create())
-            {
-                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
-                {
-                    builder.Append(bytes[i].ToString("x2"));
-                }
-                return builder.ToString();
-            }
-        }
 
         public DataTable MtdListarVendedores()
         {
