d814ede93ba63f33b0eb87d68b15b94c2c882b78
Brayam96 <165792204+Brayam96@users.noreply.github.com>
2024-11-20 23:12:56 -0500

    Mas cambiosAgrege las clases de vendedor, la modal para registrar el vendedor y el proceso en el login para darle ingreso al vendedor

---
+++ AppAtusPiesPr/Logica/ClVendedorL.cs
@@ -0,0 +1,20 @@
+﻿using AppAtusPiesPr.Datos;
+using AppAtusPiesPr.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppAtusPiesPr.Logica
+{
+    public class ClVendedorL
+    {
+        private ClVendedorD oVendedor = new ClVendedorD();
+        public int RegistrarVendedor(ClUsuarioE vendedor)
+        {
+            return oVendedor.MtdRegistrarVendedor(vendedor);
+        }
+
+
+    }
+}
