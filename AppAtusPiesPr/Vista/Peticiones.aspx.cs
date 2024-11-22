f0b6b5e06a615e7e1e8b9d9b6cce86eb00d6e0e2
santiagograna <david.granados@soy.sena.edu.co>
2024-11-21 23:01:53 -0500

    Creacion de los metodos para enviar correo electronico

--- AppAtusPiesPr/Vista/Peticiones.aspx.cs
+++ AppAtusPiesPr/Vista/Peticiones.aspx.cs
@@ -1,4 +1,5 @@
-﻿using AppAtusPiesPr.Logica;
+﻿using AppAtusPiesPr.Datos;
+using AppAtusPiesPr.Logica;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -15,6 +16,7 @@ namespace AppAtusPiesPr.Vista
         {
             if (!IsPostBack)
             {
+                // Cargar las solicitudes solo en la primera carga
                 CargarPeticiones();
             }
         }
@@ -31,12 +33,60 @@ namespace AppAtusPiesPr.Vista
 
         protected void btnAceptar_Command(object sender, CommandEventArgs e)
         {
+            try
+            {
+                int vendedorId = Convert.ToInt32(e.CommandArgument);
 
+                // Obtener el correo electrónico del vendedor
+                GridViewRow fila = (GridViewRow)((Button)sender).NamingContainer;
+                string email = fila.Cells[4].Text;
+
+                // Llamar a la lógica para aceptar la solicitud
+                ClAdminL logicaSolicitud = new ClAdminL();
+                bool exito = logicaSolicitud.MtdAceptarSolicitud(vendedorId, email);
+
+                if (exito)
+                {
+                    // Mostrar SweetAlert de éxito
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "alertaExito",
+                        "Swal.fire({ " +
+                        "title: 'Solicitud Aceptada', " +
+                        "text: 'La solicitud ha sido aceptada correctamente y se ha enviado un correo al vendedor.', " +
+                        "icon: 'success', " +
+                        "confirmButtonText: 'Aceptar' " +
+                        "});", true);
+
+                    CargarPeticiones(); // Recargar la tabla
+                }
+                else
+                {
+                    // Mostrar SweetAlert de error
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "alertaError",
+                        "Swal.fire({ " +
+                        "title: 'Error', " +
+                        "text: 'No se pudo aceptar la solicitud. Intenta de nuevo.', " +
+                        "icon: 'error', " +
+                        "confirmButtonText: 'Aceptar' " +
+                        "});", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Mostrar SweetAlert de error al capturar la excepción
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "alertaExcepcion",
+                    $"Swal.fire({{ " +
+                    $"title: 'Error', " +
+                    $"text: 'Error al aceptar la solicitud: {ex.Message}', " +
+                    $"icon: 'error', " +
+                    $"confirmButtonText: 'Aceptar' " +
+                    $"}});", true);
+            }
         }
 
-        protected void btnDenegar_Command(object sender, CommandEventArgs e)
-        {
 
-        }
+
     }
 }
+
+
+      
