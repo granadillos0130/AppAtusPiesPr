72317e839fd706ff64a861752680ed54ae777a25
Brayam96 <165792204+Brayam96@users.noreply.github.com>
2024-11-22 09:32:20 -0500

    menu y funcionamiento de cerrarsesion

--- AppAtusPiesPr/Vista/Maestra.Master.designer.cs
+++ AppAtusPiesPr/Vista/Maestra.Master.designer.cs
@@ -42,6 +42,15 @@ namespace AppAtusPiesPr.Vista
         protected global::System.Web.UI.WebControls.Label LblPuesto;
 
         /// <summary>
+        /// Control btnCerrarSesion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton btnCerrarSesion;
+
+        /// <summary>
         /// Control menuRegistrarProducto.
         /// </summary>
         /// <remarks>
