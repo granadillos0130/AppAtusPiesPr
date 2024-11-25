8850a7527c12fda74e134eecade6b580279455b5
Luis Salamanca <luisguezguann@gmail.com>
2024-11-25 08:36:55 -0500

    .

--- AppAtusPiesPr/Vista/EstadoPedido.aspx
+++ AppAtusPiesPr/Vista/EstadoPedido.aspx
@@ -18,9 +18,6 @@
                     <asp:ListItem Text="Procesado" Value="Enviado"></asp:ListItem>
                 </asp:DropDownList>
             </div>
-            <div class="col-md-2 align-self-end">
-                <asp:Button ID="btnFiltrar" runat="server" Text="Filtrar" CssClass="btn btn-primary w-100" OnClick="btnFiltrar_Click" />
-            </div>
         </div>
 
       
--- AppAtusPiesPr/Vista/EstadoPedido.aspx.designer.cs
+++ AppAtusPiesPr/Vista/EstadoPedido.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace AppAtusPiesPr.Vista
         protected global::System.Web.UI.WebControls.DropDownList ddlEstado;
 
         /// <summary>
-        /// Control btnFiltrar.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnFiltrar;
-
-        /// <summary>
         /// Control gvPedidos.
         /// </summary>
         /// <remarks>
