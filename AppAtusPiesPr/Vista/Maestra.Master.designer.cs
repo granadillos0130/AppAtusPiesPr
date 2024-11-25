36e68a9f36640fbdb159b0f76be08e819fc397a9
Luis Salamanca <luisguezguann@gmail.com>
2024-11-24 20:42:06 -0500

    Cambio estado

--- AppAtusPiesPr/Vista/Maestra.Master.designer.cs
+++ AppAtusPiesPr/Vista/Maestra.Master.designer.cs
@@ -113,16 +113,6 @@ namespace AppAtusPiesPr.Vista
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlAnchor menuListarUsuarios;
 
-
-        /// <summary>
-        /// Control menuEstadoPedido.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor menuEstadoPedido;
-
         /// <summary>
         /// Control menuEstadoPedido.
         /// </summary>
