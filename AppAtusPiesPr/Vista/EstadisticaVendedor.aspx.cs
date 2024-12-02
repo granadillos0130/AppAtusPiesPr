18c8df1ac6ac77e69a18038d02d1a36925534463
PC_15 <granadossantiago2006@gmail.com>
2024-12-02 11:23:09 -0500

    Archivos faltantes

---
+++ AppAtusPiesPr/Vista/EstadisticaVendedor.aspx.cs
@@ -0,0 +1,34 @@
+﻿using AppAtusPiesPr.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static AppAtusPiesPr.Entidades.ClUsuarioE;
+
+namespace AppAtusPiesPr.Vista
+{
+    public partial class EstadisticaVendedor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string documento = txtDocumento.Text;
+            string año = ddlAño.Text;
+            string mes = ddlMes.Text;
+
+            ClAdminL EstadisticaL = new ClAdminL();
+
+            List<ClEstadisticaVendedorE> estadisticas = EstadisticaL.MtdEstadistica(documento, año, mes);
+
+            gvEstadisticasVendedor.DataSource = estadisticas;
+            gvEstadisticasVendedor.DataBind();
+
+        }
+    }
+}
