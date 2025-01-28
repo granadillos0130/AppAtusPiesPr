72cfce3617c1183cc428eea5eb39b645a5c28feb
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2025-01-27 19:48:50 -0500

    avances en la creacion  del proceso de compras

---
+++ AppAtusPiesPr/Entidades/ClPedidosE.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppAtusPiesPr.Entidades
+{
+    public class ClPedidosE
+    {
+        public int IdPedido { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaPedido { get; set; }
+        public int IdCliente { get; set; }
+        public int IdVendedor { get; set; }
+        public decimal TotalPedido { get; set; }
+        public List<DetallePedido> Detalles { get; set; } = new List<DetallePedido>();
+    }
+
+    public class DetallePedido
+    {
+        public int IdDetallesPedido { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Subtotal => Cantidad * Precio;
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public bool DireccionPrincipal { get; set; }
+    }
+
+}
