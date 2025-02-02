0e9b2753137e35953db9f07175a73de84e1c9b5e
wilmar <perezwilmar691@gmail.com>
2025-02-02 16:31:11 -0500

    Agregue archivo   AdministrarProveedores

---
+++ AppAtusPiesPr/Vista/AdministrarProveedores.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppAtusPiesPr.Vista
+{
+    public partial class AdministrarProveedores : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
